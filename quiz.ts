5ac1fcdb5192e2c97fafb3477b1d5403ac92a985
Eric Moon <32473932+ericgmoon@users.noreply.github.com>
2026-03-18 10:41:26 +1000

    Add quiz.ts

---
+++ quiz.ts
@@ -0,0 +1,16 @@
+const allTopics: ["complex_polynomials", "vector_projections", "chemical_equilibria"]
+const allQuestions: {
+  [topic: string]: {
+	  question: string
+	  solution: string
+  }[]
+} = {
+  complex_polynomials: [{
+    question: "Solve z^2 + 1 = 0",
+    solution: "±i"
+  }]
+}
+
+const generate = () => {
+
+}
